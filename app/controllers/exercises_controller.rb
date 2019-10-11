7b865f2990587a3748353df2645b8fad04b3ee98
nicaa0695 <nicaa0695@gmail.com>
2019-10-11 05:36:12 -0600

    exercises model and controller, has many, belongs to relationship

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,7 +1,7 @@
 class ExercisesController < ApplicationController
     def create 
         @workout = Workout.find(params[:workout_id])
-        @exercise = @workout.exercises.create(params[:workout].permit(:name, :sets, :reps))
+        @exercise = @workout.exercises.create(params[:exercise].permit(:name, :sets, :reps))
 
         redirect_to workout_path(@workout)
     end 
