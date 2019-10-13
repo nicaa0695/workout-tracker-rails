cfc23df766803b5c159ba72cef8baeb654246aaa
nicaa0695 <nicaa0695@gmail.com>
2019-10-12 22:08:12 -0500

    added login with facebook- omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  get '/auth/facebook/callback' => 'sessions#create'
   resources :workouts do 
     resources :exercises
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_014721) do
+ActiveRecord::Schema.define(version: 2019_10_13_030634) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
@@ -30,8 +30,12 @@ ActiveRecord::Schema.define(version: 2019_10_13_014721) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
   create_table "workouts", force: :cascade do |t|
