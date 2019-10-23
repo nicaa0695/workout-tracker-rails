f00772d7da0082c916989a752e184218bd748c2d
nicaa0695 <nicaa0695@gmail.com>
2019-10-23 13:30:42 -0600

    added validations to users and has many through relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :email, presence: true
+  validates :email, uniqueness: :true
+  has_many :workouts, through: :exercises
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
